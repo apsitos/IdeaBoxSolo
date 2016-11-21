9e4aafd6e96dbc1e0d5754c9bd2b3f1d0ebbda0e
Annastasia Psitos <annastasia.psitos@gmail.com>
2016-11-20 18:30:54 -0700

    set localStorage function

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
-var title = $('.entered-title');
-var body = $('.entered-idea')
+var $title = $('.title');
+var $body = $('.idea');
+var ideaArray = JSON.parse(localStorage.getItem('newIdea')) || [];
+
+$('document').ready(function(){
+  getStorage();
+})
 
 $('.save-button').on('click', function() {
-  displayIdea();
+  var titleInput = $title.val();
+  var bodyInput = $body.val();
+  var newIdea = new CreateIdea(titleInput, bodyInput)
+  var id = newIdea.id;
+  var quality = newIdea.quality;
+  displayIdea(titleInput, bodyInput, id, quality);
+  ideaArray.push(newIdea);
+  saveToStorage();
+  clearForm();
 });
 
 
-function CreateIdea(id, title, body, quality) {
+function CreateIdea($title, $body, id, quality) {
   this.id = id || Date.now();
-  this.title = title;
-  this.body = body;
+  this.title = $title;
+  this.body = $body;
   this.quality = quality || 'swill';
 }
 
-function displayIdea() {
+function displayIdea(titleInput, bodyInput, id, quality) {
+  $('.idea-list').prepend(`<li id="${id}" class="new-idea"><h2 class="entered-title">${titleInput}</h2>
+    <button type="button" class="delete" name="delete" img src="../images/delete.svg"></button>
+    <h3 class="entered-idea">${bodyInput}</h3>
+    <button type="button" class="upvote" name="upvote" img src="../images/upvote.svg"></button>
+    <button type="button" class="downvote" name="downvote" img src="../images/downvote.svg"></button>
+    <p class="quality">quality: <span class="user-quality">${quality}</span></p>
+  </li>`)
+};
 
+function saveToStorage() {
+  localStorage.setItem('newIdea', JSON.stringify(ideaArray));
+};
+
+function getStorage() {
+  var storedIdeas = JSON.parse(localStorage.getItem('newIdea'));
+  if (storedIdeas) {
+    for (i = 0; i < storedIdeas.length; i++){
+      var list = storedIdeas[i];
+      displayIdea(list.title, list.body, list.id, list.quality);
+    };
+  };
+};
+
+function clearForm() {
+  var titleInput = $title.val('');
+  var bodyInput = $body.val('');
 }
